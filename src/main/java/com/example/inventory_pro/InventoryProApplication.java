358fed7f52a0f5d5b08c4c528afe1db9baf090e4
Marcos Queiroz <marcosamfq123@gmail.com>
2025-11-27 08:31:38 -0300

    Criando estrutura base do projeto

---
+++ src/main/java/com/example/inventory_pro/InventoryProApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventory_pro/InventoryProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventory_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
